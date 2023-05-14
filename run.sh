ee961fcd15c9d40d66bcb2a7b1d9352518a0c1cb
Yurchenko vladimir <vova_119@bk.ru>
2023-05-14 15:41:58 +0300

    delete other

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install virtualenv
-virtualenv venv
-source venv/bin/activate
-
-pip install aiogram
-
-python3 src/telegram_ui.py
--- src/item.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self, name: str, photo: str = '') -> None:
-        self.name = name
-        self.photo = photo
-
-    def __str__(self) -> str:
-        return self.name
